6d11b7f8515012858ea93bbf1a94ee18de8003da
Optimix Designer <optimix.designer@gmail.com>
2017-08-15 03:03:36 +0300

    Lesson 39 - Passing data between VCs

---
+++ app-swoosh/app-swoosh/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Alexander on 15.08.17.
+//  Copyright © 2017 Alexander. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+}
